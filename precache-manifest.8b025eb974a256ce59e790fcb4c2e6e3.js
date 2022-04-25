22ef29a97762bfe4db3f0f2beaa977eb4b789459
yume <785879986@qq.com>
2022-04-25 14:52:26 +0800

    deploy

---
+++ precache-manifest.8b025eb974a256ce59e790fcb4c2e6e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41897e9e3fbdfe674d58",
+    "url": "/qianji-website/css/app.f5a5b009.css"
+  },
+  {
+    "revision": "aa35cb36f7819c8b477f2ba4f823e009",
+    "url": "/qianji-website/index.html"
+  },
+  {
+    "revision": "41897e9e3fbdfe674d58",
+    "url": "/qianji-website/js/app.38ed8e08.js"
+  },
+  {
+    "revision": "2e651c9f2fe16126693c",
+    "url": "/qianji-website/js/chunk-vendors.3b922e31.js"
+  },
+  {
+    "revision": "6f322767b9edcbfd5467509c1b293753",
+    "url": "/qianji-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/qianji-website/robots.txt"
+  }
+]);
